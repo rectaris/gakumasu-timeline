e5ea07adc02d7fb038628b22613a2f3576416973
rectaris <furi107k@gmail.com>
2026-01-18 02:13:45 +0900

    events update

--- src/data/events.js
+++ src/data/events.js
@@ -7,6 +7,14 @@ export const characters = [
         { year: 1, month: 1, title: "冒険開始", detail: "〇〇が発生、△と出会う" },
         { year: 2, month: 6, title: "大事件", detail: "□□が崩壊する" }
       ]
+    },
+    {
+        id: "heroin",
+        name: "ヒロイン",
+        color: "#3b8200",
+        events: [
+          { year: 1, month: 5, title: "冒険開始", detail: "主人公と出会う" },
+        ]
     }
-  ];
+];
   
