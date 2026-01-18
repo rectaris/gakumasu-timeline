82c7cf41496bbe3f96371000c98ea69dc0e77ea1
rectaris <furi107k@gmail.com>
2026-01-18 19:44:11 +0900

    saki event update

--- src/data/characters/001hanamiSaki.js
+++ src/data/characters/001hanamiSaki.js
@@ -4,11 +4,18 @@ export default {
   color: "#ff0000", // 赤（イメージカラー）
   events: [
     {
+      id: "saki_birth",
+      start: { year: -16, month: 4 },
+      end: { year: -16, month: 4 },
+      title: "誕生",
+      detail: "花海咲季、誕生"
+    },
+    {
       id: "saki_debut",
-      start: { year: 1, month: 1 },
-      end: { year: 1, month: 1 },
+      start: { year: 1, month: 4 },
+      end: { year: 1, month: 4 },
       title: "初星学園入学",
-      detail: "初星学園に入学しアイドル候補として活動開始"
+      detail: "初星学園に入学、1年1組に配属される"
     }
   ]
 };
